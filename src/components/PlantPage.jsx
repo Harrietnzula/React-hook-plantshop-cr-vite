ab1906f7bb2be1f3a4d3091290a107e93bed30b2
Harrietnzula <harrinzush@gmail.com>
2026-05-13 13:54:03 +0300

    fix: remove accidental git status from PlantPage.jsx

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -6,7 +6,6 @@ import Search from "./Search";
 function PlantPage() {
   const [plants, setPlants] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-git status
   const displayedPlants = plants.filter((plant) => {
   return plant.name.toLowerCase().includes(searchTerm.toLowerCase());
 });
